be5b097571c645859db3e291c8a3e0220b8f801d
Lucian Daniliuc <dlucian@gmail.com>
2021-11-06 19:29:39 +0000

    add router code to get() and match() routes
    and make the first test pass

--- src/Router.php
+++ src/Router.php
@@ -2,15 +2,37 @@
 
 namespace App;
 
+/**
+ * Class Router
+ *
+ * @package Storlex
+ * @author  Lucian Daniliuc <dlucian@gmail.com>
+ * @version 1.0
+ */
 class Router
 {
-    public function get(string $path, string $class): string
+    /**
+     * @var array<string, array<string, string>>
+     */
+    protected $routes = [];
+
+    public function get(string $path, string $class): void
     {
-        return 'get';
+        $this->addRoute('GET', $path, $class);
     }
 
-    public function match(string $path): string
+    protected function addRoute(string $method, string $path, string $class): void
     {
-        return 'match';
+        $this->routes[$method][$path] = $class;
+    }
+
+    public function match(string $path): ?string
+    {
+        foreach ($this->routes as $method => $routes) {
+            if (array_key_exists($path, $routes)) {
+                return $routes[$path];
+            }
+        }
+        return null;
     }
 }
