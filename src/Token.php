8b168df790675c3666206ec9f13ba2820e8145ae
Lucian Daniliuc <dlucian@gmail.com>
2021-11-08 21:44:13 +0000

    add revoke token

--- src/Token.php
+++ src/Token.php
@@ -4,13 +4,45 @@ namespace App;
 
 use PDO;
 
+/**
+ * Token handling class
+ *
+ * Used to generate, validate and delete tokens
+ * that are used to authenticate users.
+ */
 class Token
 {
+    /**
+     * Grant access to $token
+     *
+     * @param string $token
+     * @return void
+     */
     public function grant(string $token): void
     {
+        if ($this->isValid($token)) {
+            return; // Already granted
+        }
         Database::execute('INSERT INTO tokens (token) VALUES (:token)', [ 'token' => $token ]);
     }
 
+    /**
+     * Remoke access to $token
+     *
+     * @param string $token
+     * @return void
+     */
+    public function revoke(string $token): void
+    {
+        Database::execute('DELETE FROM tokens WHERE token = :token', [ 'token' => $token ]);
+    }
+
+    /**
+     * Check if $token is valid
+     *
+     * @param string $token Token to check
+     * @return bool
+     */
     public function isValid(string $token): bool
     {
         $results = Database::query('SELECT * FROM tokens WHERE token = :token', [ 'token' => $token ]);
--- tests/TokenTest.php
+++ tests/TokenTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Database;
+use App\Token;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -12,14 +13,15 @@ class TokenTest extends TestCase
 {
     public function setUp(): void
     {
-        Database::execute('CREATE TABLE IF NOT EXISTS tokens (token VARCHAR(100) NOT NULL)');
+        Database::execute('CREATE TABLE IF NOT EXISTS tokens (token VARCHAR(100) NOT NULL PRIMARY KEY)');
+        Database::execute('DELETE FROM tokens');
     }
 
     /** @test */
     public function itCreatesAToken()
     {
         // Arrange
-        $token = new \App\Token();
+        $token = new Token();
 
         // Act
         $token->grant('foo');
@@ -32,12 +34,41 @@ class TokenTest extends TestCase
     public function itValidatesAPreviouslyCreatedToken()
     {
         // Arrange
-        $token = new \App\Token();
+        $token = new Token();
 
         // Act
         $token->grant('bar');
 
         // Assert
         $this->assertFalse($token->isValid('foo'));
+    }
+
+    /** @test */
+    public function itRevokesAToken()
+    {
+        // Arrange
+        $token = new Token();
+        $token->grant('baz');
+        $this->assertTrue($token->isValid('baz'));
+
+        // Act
+        $token->revoke('baz');
+
+        // Assert
+        $this->assertFalse($token->isValid('baz'));
+    }
+
+    /** @test */
+    public function itCanGrantATokenThatWasAlreadyGranted()
+    {
+        // Arrange
+        $token = new Token();
+
+        // Act
+        $token->grant('fooBar');
+        $token->grant('fooBar');
+
+        // Assert
+        $this->assertTrue($token->isValid('fooBar'));
     }
 }
