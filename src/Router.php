045aac11319ad2bbd8065d571036e894b7ef9555
Lucian Daniliuc <dlucian@gmail.com>
2021-11-08 18:05:01 +0000

    make router handle closures

--- src/Router.php
+++ src/Router.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Closure;
+
 /**
  * Class Router
  *
@@ -12,25 +14,35 @@ namespace App;
 class Router
 {
     /**
-     * @var array<string, array<string, string>>
+     * @var array<string, array<string, Closure>>
      */
     protected $routes = [];
 
-    public function get(string $path, string $class): void
+    /**
+     * Add a GET route for $path to $handler
+     */
+    public function get(string $path, Closure $handler): void
     {
-        $this->addRoute('GET', $path, $class);
+        $this->addRoute('GET', $path, $handler);
     }
 
-    protected function addRoute(string $method, string $path, string $class): void
+    protected function addRoute(string $method, string $path, Closure $handler): void
     {
-        $this->routes[$method][$path] = $class;
+        $this->routes[$method][$path] = $handler;
     }
 
-    public function match(string $path): ?string
+    public function match(string $path): ?Closure
     {
+        $path = (string)filter_var($path, FILTER_SANITIZE_URL);
         foreach ($this->routes as $method => $routes) {
             if (array_key_exists($path, $routes)) {
                 return $routes[$path];
+            }
+            foreach ($routes as $pathSearch => $handler) {
+                $pathRegexp = preg_replace('/({)([a-zA-Z0-9]+)(})/', '(?<$2>.+)', $pathSearch);
+                if (preg_match('#^' . $pathRegexp . '$#', $path)) {
+                    return $handler;
+                }
             }
         }
         return null;
--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -7,10 +7,59 @@ use PHPUnit\Framework\TestCase;
 
 final class RouterTest extends TestCase
 {
-    public function testRouterServesHomeController()
+    public function testRouterMatchesRootPath()
     {
+        // Arrange
+        $callback = function () {
+            return 'Hello World';
+        };
+
+        // Act
         $router = new Router();
-        $router->get('/', 'HomeController@index');
-        $this->assertEquals('HomeController@index', $router->match('/'));
+        $router->get('/', $callback);
+
+        // Assert
+        $this->assertEquals($callback, $router->match('/'));
+    }
+
+    public function testRouterMatchesPathWithParameters()
+    {
+        // Arrange
+        $callbackUserShow = function ($id) {
+            return 'User: ' . $id;
+        };
+        $callbackImageShow = function ($id) {
+            return 'Image: ' . $id;
+        };
+        $callbackUnmatched = function () {
+            return 'Unmatched';
+        };
+
+        // Act
+        $router = new Router();
+        $router->get('/users/{id}', $callbackUserShow);
+        $router->get('/image/{slug}', $callbackImageShow);
+        $router->get('/unmatched/{id}', $callbackUnmatched);
+
+        // Assert
+        $this->assertEquals($callbackUserShow, $router->match('/users/1'));
+        $this->assertEquals($callbackImageShow, $router->match('/image/john'));
+        $this->assertEquals(null, $router->match('/unmatched/'));
+    }
+
+    public function testRouterOnlyMatchesAsciiPaths()
+    {
+        // Arrange
+        $callback = function () {
+            return 'Hello World';
+        };
+
+        // Act
+        $router = new Router();
+        $router->get('/sõmethiñg', $callback);
+
+        // Assert
+        $this->assertEquals(null, $router->match('/sõmethiñg'));
+        $this->assertEquals(null, $router->match('/something'));
     }
 }
