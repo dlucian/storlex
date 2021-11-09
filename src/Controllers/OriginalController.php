40eab0dc014e73b6214be6e7ee12566887224e3c
Lucian Daniliuc <dlucian@gmail.com>
2021-11-09 22:18:07 +0000

    return upload filename in the response

--- src/Controllers/OriginalController.php
+++ src/Controllers/OriginalController.php
@@ -6,11 +6,26 @@ use App\Drivers\DriverManager;
 use App\Request;
 use App\Response;
 
+/**
+ * Original Images Controller
+ *
+ * Handles uplaoding of original images,
+ * which are to be resized and optimized.
+ */
 class OriginalController extends BaseController
 {
+    /**
+     * Upload image
+     *
+     * @param Request $request
+     * @return Response
+     */
     public function upload(Request $request): Response
     {
-        DriverManager::imageStorage()->save($request->getFile('file'));
-        return $this->successJson('File uploaded successfully');
+        $file = $request->getFile('file');
+        DriverManager::imageStorage()->save($file);
+        return $this->successJson(
+            sprintf('File \'%s\' uploaded successfully', $file['name'])
+        );
     }
 }
